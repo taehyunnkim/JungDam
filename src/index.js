969f37ba2b250cd7db7a8b54ca8a91d948fbfd4f
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2021-05-30 09:36:25 +0900

    Attempt IE fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'core-js/stable'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
