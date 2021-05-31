cdb4b8c5c78fdbfcf514523306e5c418916e06b2
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2021-06-01 00:07:12 +0900

    Remove duplicate image and clean up html

--- src/components/layout/Gallery.js
+++ src/components/layout/Gallery.js
@@ -17,7 +17,7 @@ import pic15 from "../../images/gallery/15.jpg";
 import pic16 from "../../images/gallery/16.jpg";
 import pic17 from "../../images/gallery/17.jpg";
 import pic18 from "../../images/gallery/18.jpg";
-import pic19 from "../../images/gallery/1.jpg";
+// import pic19 from "../../images/gallery/19.jpg";
 import '../../scss/Gallery.scss';
 import { useEffect } from 'react';
 
@@ -49,7 +49,7 @@ function Gallery({setPage}) {
                 <img src={pic16} alt="gallery"/>
                 <img src={pic17} alt="gallery"/>
                 <img src={pic18} alt="gallery"/>
-                <img src={pic19} alt="gallery"/>
+                {/* <img src={pic19} alt="gallery"/> */}
             </div>
         </div>
     );
