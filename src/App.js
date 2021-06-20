6cc1f92ff553239e00e0ff547100589898ca6ead
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2021-06-20 14:52:45 +0900

    Add react helmet async

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from './components/layout/Landing';
 import Menu from './components/layout/Menu';
 import Gallery from './components/layout/Gallery';
 import Footer from './components/layout/Footer';
+import {Helmet, HelmetProvider} from 'react-helmet-async';
 import {
   BrowserRouter,
   Switch,
@@ -31,34 +32,57 @@ function App() {
   
   return (
     <BrowserRouter>
-      <div className="masterContainer">
-      {page !== "home" ? 
-        <div id="lines" className="full">
-          <div className="bgLine leftLine"></div>
-          <div className="bgLine middleLine"></div>
-          <div className="bgLine rightLine"></div>
-        </div>
-          : 
-        <div id="lines">
-          <div className="bgLine middleLine"></div>
-        </div>
-        }
+      <HelmetProvider>
+        <div className="masterContainer">
+        {page !== "home" ? 
+          <div id="lines" className="full">
+            <div className="bgLine leftLine"></div>
+            <div className="bgLine middleLine"></div>
+            <div className="bgLine rightLine"></div>
+          </div>
+            : 
+          <div id="lines">
+            <div className="bgLine middleLine"></div>
+          </div>
+          }
 
-        <Nav showNav={showNav} page={page} setMenu={setMenu} />
-        <Switch>
-          <Route path="/menu">
-            <Menu setPage={setPage} menu={menu} />
-          </Route>
-          <Route path="/gallery">
-            <Gallery setPage={setPage} />
-          </Route>
-          <Route exact path="/">
-            <Landing setPage={setPage} />
-          </Route>
-          <Redirect from="*" to="/" />
-        </Switch>
-        <Footer />
-      </div>
+          <Nav showNav={showNav} page={page} setMenu={setMenu} />
+          <Switch>
+            <Route path="/menu">     
+              <Helmet>
+                  <title>정담 메뉴</title>
+                  <meta
+                    name="description"
+                    content="늘 신선하고 깨끗한 맛으로 건강까지 채워드리겠습니다."
+                  />
+              </Helmet>
+              <Menu setPage={setPage} menu={menu} />
+            </Route>
+            <Route path="/gallery">
+              <Helmet>
+                  <title>정담 갤러리</title>
+                  <meta
+                    name="description"
+                    content="청결한 위생으로 안전한 먹거리를 제공하겠습니다."
+                  />
+              </Helmet>
+              <Gallery setPage={setPage} />
+            </Route>
+            <Route exact path="/">
+              <Helmet>
+                <title>정담도시락</title>
+                <meta
+                  name="description"
+                  content="행복을 담은 도시락, 정성을 담은 도시락으로 건강과 행복을 드리겠습니다."
+                />
+              </Helmet>
+              <Landing setPage={setPage} />
+            </Route>
+            <Redirect from="*" to="/" />
+          </Switch>
+          <Footer />
+        </div>
+      </HelmetProvider>
     </BrowserRouter>
   );
 }
